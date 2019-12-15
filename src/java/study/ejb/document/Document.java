ecfb63c3131fe732ea26124849a60ea22cfb4689
Urbanovich Dzmitry <urbanovich.mslo@gmail.com>
2019-12-15 23:26:41 +0300

    moved business login to ejb

--- src/java/study/ejb/document/Document.java
+++ src/java/study/ejb/document/Document.java
@@ -5,7 +5,9 @@
  */
 package study.ejb.document;
 
+import java.util.List;
 import javax.ejb.Stateless;
+import study.ejb.document.helpers.Reader;
 
 /**
  *
@@ -16,4 +18,43 @@ public class Document implements DocumentRemote {
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+        
+    public static Reader reader;
+    
+    public Document() {        
+        this.reader = new Reader(
+                            "study/web/document/resources/documents.xml", 
+                            "study/web/document/resources/documents.xsd"
+                        );
+        
+        this.reader.read();
+    }
+    
+    @Override
+    public List<study.ejb.document.entity.Document> getList() {
+        return this.reader.getList();
+    }
+
+    @Override
+    public study.ejb.document.entity.Document search(String id) {
+        return this.reader.search(id);
+    }
+
+    @Override
+    public boolean add(int id, String title, String content) {
+        return this.reader.add(id, title, content);
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return this.reader.delete(id);
+    }
+
+    @Override
+    public void save() {
+        this.reader.save();
+    }
+    
+    
+    
 }
